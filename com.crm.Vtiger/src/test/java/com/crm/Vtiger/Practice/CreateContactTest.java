b66e78b7a64346be56ddb88aa067bcbbcc85b896
Aman <Aman@hp>
2022-03-25 07:35:55 +0530

    2nd new workspaces 1st commit

---
+++ com.crm.Vtiger/src/test/java/com/crm/Vtiger/Practice/CreateContactTest.java
@@ -0,0 +1,13 @@
+package com.crm.Vtiger.Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void createContact()
+	{
+		System.out.println("Contact Created");
+	}
+	
+}

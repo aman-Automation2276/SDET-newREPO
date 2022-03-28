3cf7f91cbfc2af96901ae84c7c97b45edcedfd2b
Aman <Aman@hp>
2022-03-28 22:06:29 +0530

    User1 commit

---
+++ com.crm.Vtiger/src/test/java/com/crm/Vtiger/Practice/CreateOwnerTest.java
@@ -0,0 +1,12 @@
+package com.crm.Vtiger.Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateOwnerTest {
+
+	@Test
+	public void createOwner()
+	{
+		System.out.println("Owner created");
+	}
+}

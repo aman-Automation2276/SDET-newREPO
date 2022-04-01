4ed1e3d2ba63286dc4422eacdee3fbbf2ae18a78
aman-Automation2276 <aman2276@gmail.com>
2022-04-01 14:36:13 +0530

    Invoice user1

---
+++ com.crm.Vtiger/src/test/java/com/crm/Vtiger/Practice/CreateInvoiceByUser1Test.java
@@ -0,0 +1,12 @@
+package com.crm.Vtiger.Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateInvoiceByUser1Test {
+
+	@Test
+	public void createInvoice()
+	{
+		System.out.println("Invoice created by user 1");
+	}
+}
